9e993aaca7328f837d7b9e04b26527ada9d7865d
Bogus Hawtsauce <evad@lot224.net>
2016-03-23 13:55:07 -0400

    Completed Chapter 4

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,37 @@
-import {Component} from 'angular2/core';
+﻿import { Component } from 'angular2/core';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+import { HeroService } from './hero.service';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+  selector:'my-app',
+  templateUrl: 'app/app.component.html',
+  styleUrls: [ 'app/app.component.css' ],
+  directives:[ROUTER_DIRECTIVES],
+  providers:[ROUTER_PROVIDERS, HeroService]
 })
-export class AppComponent { }
+
+@RouteConfig([
+  {
+    path: '/heroes',
+    name: 'Heroes',
+    component: HeroesComponent
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+  },
+  {  
+    path: '/detail/:id',
+    name: 'HeroDetail',
+    component: HeroDetailComponent
+  },
+])
+
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
